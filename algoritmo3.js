f93083160b659e4f4a49c4c8140104224f6b4688
Yoel <yoelmontelongoo0@gmail.com>
2025-04-29 01:27:31 -0300

    Add files via upload

---
+++ algoritmo3.js
@@ -0,0 +1,31 @@
+// contar los carecteres de 'a' en el string
+
+function filtrarRepetidas (string) {
+
+    let repetidas = 0;
+
+    for(let i = 0; i <= string.length; i++){
+        if(string[i] === 'b'){
+            repetidas +=1
+        }
+    }
+    return repetidas;
+}
+console.log(filtrarRepetidas("beisbol"));
+console.log(filtrarRepetidas("Balón"));
+console.log(filtrarRepetidas("pasto"));
+console.log(filtrarRepetidas("bombilla"));
+
+
+// encontrar palabras que comiencen con la letra 'T'
+
+const animales = ['Pantera','Tigre','Gato','Perro',
+                    'Tiburon','Tortuga','Toro'];
+
+function elementos (condicion){
+    return condicion.startsWith('T');
+}
+
+const animalesT = animales.filter(elementos)
+console.log(animalesT);
+
